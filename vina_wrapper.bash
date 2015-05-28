b16e7a62afd03db6267e6c9fcbfb61b431ec9a34
Bala Desinghu <dmbala@gmail.com>
2015-05-28 15:37:04 -0500

    Update vina_wrapper.bash
    changing the output names

--- vina_wrapper.bash
+++ vina_wrapper.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 module load autodock 
-vina --config receptor_config.txt --ligand ligand.pdbqt --out receptor-ligand_output.pdbqt --log receptor-ligand_ligand.log
+vina --config receptor_config.txt --ligand ligand.pdbqt --out receptor-ligand.pdbqt --log receptor-ligand.log
