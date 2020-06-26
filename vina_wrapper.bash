8558d287b9e14172b42a489f9b8ef518fb693037
Christina K <christinakconnect@gmail.com>
2020-06-26 11:33:45 -0500

    updates for downloading vina

--- vina_wrapper.bash
+++ vina_wrapper.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-module load autodock 
-vina --config receptor_config.txt --ligand ligand.pdbqt --out receptor-ligand.pdbqt --log receptor-ligand.log
+# Unzip autodock vina software
+tar -xzf VINA_TGZ
+export PATH=$PWD/VINA_DIR/bin:$PATH
+
+# Run vina
+vina --config receptor_config.txt \
+     --ligand ligand.pdbqt --out receptor-ligand.pdbqt \
+     --log receptor-ligand.log
