47e7c009739aedb3f9bdaaf3c9b6a4006368309f
Christina Koch <christinakconnect@gmail.com>
2024-03-21 11:23:47 -0500

    updating multi-job scripts

--- vina_multi.sh
+++ vina_multi.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-VINA_DIR=$1
-
-# Unzip autodock vina software into a folder called autodock
-tar -xzf autodock_vina.tar.gz
-export PATH=$PWD/${VINA_DIR}/bin:$PATH
-
 # Make a directory for results
 mkdir results
 
 # Run vina on multiple ligands
 for LIGAND in *ligand.pdbqt
 do 
-vina --config receptor_config.txt \
-     --ligand ${LIGAND} --out results/receptor-${LIGAND} \
-     --log results/receptor-${LIGAND}.log
+./vina --config receptor_config.txt \
+     --ligand ${LIGAND} --out results/receptor-${LIGAND}
 done
