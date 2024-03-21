a637820d16b3fc3a653a74de718fa229b7dd47eb
Christina Koch <christinakconnect@gmail.com>
2024-03-21 11:23:37 -0500

    changing files to use binary

--- vina_run.sh
+++ vina_run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-VINA_DIR=$1
-
-# Unzip autodock vina software into a folder called autodock
-tar -xzf autodock_vina.tar.gz
-export PATH=$PWD/${VINA_DIR}/bin:$PATH
-
-# Make a directory for results
-mkdir results
-
 # Run vina
 vina --config receptor_config.txt \
-     --ligand ligand.pdbqt --out results/receptor-ligand.pdbqt \
-     --log results/receptor-ligand.log
+     --ligand ligand.pdbqt --out results/receptor-ligand.pdbqt
